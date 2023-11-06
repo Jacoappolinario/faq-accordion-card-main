5b06a0e0063b551f8a1c295d33192f49dfea2f15
Jacó Apolinário <53705089+Jacoappolinario@users.noreply.github.com>
2023-11-06 17:29:50 -0300

    refactor script

--- js/script.js
+++ js/script.js
@@ -4,23 +4,39 @@ const answers = document.querySelectorAll(".answer");
 questions.forEach((question) => {
   question.addEventListener("click", (event) => {
     const clickedQuestion = event.currentTarget;
+    const correspondingAnswer = getCorrespondingAnswer(clickedQuestion);
 
-    const clickedQuestionControls =
-      clickedQuestion.getAttribute("aria-controls");
-    const correspondingAnswer = document.getElementById(
-      clickedQuestionControls
-    );
+    toggleQuestionAndAnswerVisibility(clickedQuestion, correspondingAnswer);
 
-    correspondingAnswer.classList.toggle("active");
-
-    const isAnswerActive = correspondingAnswer.classList.contains("active");
-
-    clickedQuestion.setAttribute("aria-expanded", isAnswerActive);
-
-    answers.forEach((answer) => {
-      if (answer !== correspondingAnswer) {
-        answer.classList.remove("active");
-      }
-    });
+    removeActiveClassFromElements(clickedQuestion, questions);
+    removeActiveClassFromElements(correspondingAnswer, answers);
   });
 });
+
+function getCorrespondingAnswer(clickedQuestion) {
+  const clickedQuestionControls = clickedQuestion.getAttribute("aria-controls");
+
+  const correspondingAnswer = document.getElementById(clickedQuestionControls);
+
+  return correspondingAnswer;
+}
+
+function toggleQuestionAndAnswerVisibility(
+  clickedQuestion,
+  correspondingAnswer
+) {
+  clickedQuestion.classList.toggle("active");
+  correspondingAnswer.classList.toggle("active");
+
+  const isAnswerActive = correspondingAnswer.classList.contains("active");
+
+  clickedQuestion.setAttribute("aria-expanded", isAnswerActive);
+}
+
+function removeActiveClassFromElements(activeElement, elements) {
+  elements.forEach((element) => {
+    if (element !== activeElement) {
+      element.classList.remove("active");
+    }
+  });
+}
